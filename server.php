81f2234c15b234833e80e9060b425e6afb0d7e63
unknown <kevin7776382@gmail.com>
2018-03-05 18:31:50 +0800

    fix

--- server.php
+++ server.php
@@ -5,7 +5,7 @@ function postToDiscord($info, $name)
     $data = array(
     "content" => $info, 
     "username" => $name);
-    $curl = curl_init("https://discordapp.com/api/webhooks/334989487824437258/1bovUYRZo56y8wZTqIqIv_moxgOYRg4ac1fs2eJvn3F2WoHPrxAte5oQiIJZlO4wOD13");
+    $curl = curl_init("Web_Hook_API_URL");
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -16,7 +16,7 @@ if(!empty($_POST['message'])){
         $info = $_POST['message'];
         $name = $_POST['username'];
         postToDiscord($info,$name);
-        echo $name . '的訊息已傳送至Discord#main';
+        echo $name . '的訊息已傳送';
 }
 
 ?>
